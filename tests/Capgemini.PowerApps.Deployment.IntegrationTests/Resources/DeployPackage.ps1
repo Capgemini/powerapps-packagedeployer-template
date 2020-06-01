559e1cda5b6b750a6cfc23c7b399d1cc29da904c
Ewing, Max <max.ewing@capgemini.com>
2020-06-01 09:30:26 +0000

    Merged PR 574: Updated to use Package Deployer from PowerShell Gallery
    NuGet package was deprecated.

--- tests/Capgemini.PowerApps.Deployment.IntegrationTests/Resources/DeployPackage.ps1
+++ tests/Capgemini.PowerApps.Deployment.IntegrationTests/Resources/DeployPackage.ps1
@@ -2,12 +2,7 @@ $ErrorActionPreference = "Stop"
 
 Set-Location "$PSScriptRoot\.."
 
-robocopy /xc /xn /xo /s "." "../PackageDeployer"
-
-Set-Location "../PackageDeployer"
-
-Import-Module .\Microsoft.Xrm.Tooling.PackageDeployment.Powershell.dll -Force
-Import-Module .\Microsoft.Xrm.Tooling.Connector.dll -Force
+Install-Module -Name Microsoft.Xrm.Tooling.PackageDeployment.Powershell -Force
 
 $connectionString = "Url=$env:CAPGEMINI_PACKAGE_DEPLOYER_TESTS_URL; Username=$env:CAPGEMINI_PACKAGE_DEPLOYER_TESTS_USERNAME; Password=$env:CAPGEMINI_PACKAGE_DEPLOYER_TESTS_PASSWORD; authtype=Office365"
 
