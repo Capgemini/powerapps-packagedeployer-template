3460b876ea7e2b7690d8e10ae29a009803602293
Mark Cunningham <mark_cunningham@outlook.com>
2021-01-04 10:40:41 +0000

    Added flow solution and config

---
+++ src/Capgemini.PowerApps.PackageDeployerTemplate/Config/FlowConfig.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Xml.Serialization;
+
+namespace Capgemini.PowerApps.PackageDeployerTemplate.Config
+{
+    public class FlowConfig
+    {
+        /// <summary>
+        /// The name of the connection.
+        /// </summary>
+        [XmlAttribute("flowConnectionName")]
+        public string FlowSharedConnectionName { get; set; }
+
+        /// <summary>
+        /// If true, Activates the flow.
+        /// </summary>
+        [XmlAttribute("activateFlow")]
+        public bool ActivateFlow { get; set; } = true;
+
+        public string EnvironmentVariablePrefix { get; set; } = "CAPGEMINI_PACKAGEDEPLOYER_CONNECTION_{0}";
+
+        public Guid WorkFlowId { get; internal set; }
+    }
+}
