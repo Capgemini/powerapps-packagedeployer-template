7da3928e87f41973a9034cde9db8c22c2b126b50
Max Ewing <max.ewing@capgemini.com>
2021-01-23 01:14:12 +0000

    refactor: include solution source in package
    This commit also removes some of the dependencies that the old test package had on Dynamics 365 modules (i.e. case entity)

--- tests/Capgemini.PowerApps.PackageDeployerTemplate.MockPackage/MockPackage.cs
+++ tests/Capgemini.PowerApps.PackageDeployerTemplate.MockPackage/MockPackage.cs
@@ -1,8 +1,12 @@
+using Microsoft.Xrm.Tooling.PackageDeployment.CrmPackageExtentionBase;
+using System.ComponentModel.Composition;
+
 namespace Capgemini.PowerApps.PackageDeployerTemplate.MockPackage
 {
     /// <summary>
     /// A mock package used for testing. 
     /// </summary>
+    [Export(typeof(IImportExtensions))]
     public class MockPackage : PackageTemplateBase
     {
         public override string GetLongNameOfImport => "Mock Package";
