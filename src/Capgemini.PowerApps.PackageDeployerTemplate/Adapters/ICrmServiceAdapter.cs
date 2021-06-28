81e75bbd9b7b6b43994794feba4f963e7ac2aab0
Tom Ashworth <tom.ashworth@capgemini.com>
2021-06-28 13:32:04 +0100

    Update method docs

--- src/Capgemini.PowerApps.PackageDeployerTemplate/Adapters/ICrmServiceAdapter.cs
+++ src/Capgemini.PowerApps.PackageDeployerTemplate/Adapters/ICrmServiceAdapter.cs
@@ -21,7 +21,7 @@
         /// Imports a word template.
         /// </summary>
         /// <param name="fileInfo">Information about file.</param>
-        /// <param name="entityLogicalName">entity location name/schema name.</param>
+        /// <param name="entityLogicalName">The entity schema name the document is based off.</param>
         /// <param name="templateType">template extension type.</param>
         /// <param name="filePath">The path to the word template.</param>
         void ImportWordTemplate(FileInfo fileInfo, string entityLogicalName, OptionSetValue templateType, string filePath);
