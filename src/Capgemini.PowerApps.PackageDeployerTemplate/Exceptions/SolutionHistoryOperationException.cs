c9cf157aa299ffaa6946c5284cdf78ad84a99fb4
Iain Lomax <iain.lomax@capgemini.com>
2024-10-23 09:51:36 +0100

    feat: add custom SolutionHistoryOperationException for handling solution history operation errors #89

---
+++ src/Capgemini.PowerApps.PackageDeployerTemplate/Exceptions/SolutionHistoryOperationException.cs
@@ -0,0 +1,19 @@
+﻿namespace Capgemini.PowerApps.PackageDeployerTemplate.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Represents an exception that is thrown when an operation related to Solution History fails.
+    /// </summary>
+    public class SolutionHistoryOperationException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SolutionHistoryOperationException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public SolutionHistoryOperationException(string message)
+            : base(message)
+            {
+            }
+    }
+}
