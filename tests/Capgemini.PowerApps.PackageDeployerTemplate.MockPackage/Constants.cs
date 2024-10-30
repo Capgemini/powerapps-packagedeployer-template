44eb9b3a9dc02eaaecc9cc086a5a2d339e343c48
Iain Lomax <iain.lomax@capgemini.com>
2024-10-30 16:38:13 +0000

    feat: add constants to store solution names

---
+++ tests/Capgemini.PowerApps.PackageDeployerTemplate.MockPackage/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Capgemini.PowerApps.PackageDeployerTemplate.MockPackage
+{
+    public static class Constants
+    {
+        public static class Solutions
+        {
+            public const string ActiveSolutionHistory = "pdt_ActiveSolutionHistory_managed";
+        }
+    }
+}
