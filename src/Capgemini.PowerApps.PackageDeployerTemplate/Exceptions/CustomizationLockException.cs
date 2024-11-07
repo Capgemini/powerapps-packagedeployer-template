0f97d31867959a35f4337d64a29c79e1b4f453f4
Iain Lomax <iain.lomax@capgemini.com>
2024-11-07 14:44:11 +0000

    feat: add exception types customization lock and solution concurrency

---
+++ src/Capgemini.PowerApps.PackageDeployerTemplate/Exceptions/CustomizationLockException.cs
@@ -0,0 +1,19 @@
+﻿namespace Capgemini.PowerApps.PackageDeployerTemplate.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// Represents an exception that is thrown when a customization lock occurs.
+    /// </summary>
+    public class CustomizationLockException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomizationLockException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public CustomizationLockException(string message)
+            : base(message)
+        {
+        }
+    }
+}
